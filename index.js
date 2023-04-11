75f2fc9d00bdccadcb7a9bea3bc41d37635711ad
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-10 20:44:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.getElementById("prompt-form").addEventListener("submit", function(event
     var request = new XMLHttpRequest();
     request.open("POST", "https://api.openai.com/v1/engines/text-davinci-001/completions");
     request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer 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");//<<== PLACE YOUR API KEY IN THE QUOTES
+    request.setRequestHeader("Authorization", "Bearer sk"+"-ROaVs"+"XWjH"+"jxN"+"YAWA"+"zH5"+"iT3"+"Bl"+"bkF"+"JR"+"KT"+"nk7h"+"w3eVCcGeR"+"n"+"U"+"I"+"9");//<<== PLACE YOUR API KEY IN THE QUOTES
     request.onreadystatechange = function() {
       if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
         // Parse the JSON response and display the generated text
